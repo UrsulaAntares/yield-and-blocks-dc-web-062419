3e88dba888d59a056e8888976d901a8d3193fa17
Ursula Miller <ursula.miller@gmail.com>
2019-06-16 21:21:46 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,20 +1,20 @@
 
 
 
-def hello_t(array)
- if block_given?
-    i = 0
-    while
-      i < array.length
-      yield array[i]
-      i = i + 1
-    end
+#def hello_t(array)
+# if block_given?
+#    i = 0
+#    while
+#      i < array.length
+#      yield array[i]
+#      i += 1
+#    end
     
-    array
-  else 
-    puts "Hey! No block was given!"
-  end
-end
+#    array
+#  else 
+#    puts "Hey! No block was given!"
+#  end
+#end
 
 # call your method here!
 
@@ -27,3 +27,34 @@ end
 #    puts "Hi, #{name}"
 #  end
 #end
+
+
+
+#def yielding_with_arguments(num)
+#  puts "the program is executing the code inside the method"
+#  yield(27)
+#  puts "now we are back in the method"
+#end
+
+#yielding_with_arguments(64) {|i| puts i * 2}
+
+def hello_t(array)
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
+
+array= ["Tim", "Tom", "Jim"]
+
+hello_t(array) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
